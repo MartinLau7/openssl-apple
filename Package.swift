bd91795c3aaddf883b36b5f7e0f21ed241cd67e0
Martin Lau <martinlau@livemail.tw>
2022-04-08 17:57:55 +0800

    update SPM version to 5.6

--- Package.swift
+++ Package.swift
@@ -1,19 +1,29 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "openssl-apple",
-    platforms: [.macOS("11")],
+	platforms: [
+		.macOS("11")
+	],
+	products: [
+		// Products define the executables and libraries a package produces, and make them visible to other packages.
+		.executable(
+			name: "openssl",
+			targets: [
+				"openssl-apple"
+			]
+		),
+	],
     dependencies: [
-        .package(url: "https://github.com/yury/FMake", from : "0.0.6"),
-    //    .package(path: "../FMake")
+        .package(url: "https://github.com/yury/FMake", from : "0.0.18")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .executableTarget(
             name: "openssl-apple",
             dependencies: ["FMake"]),
     ]
